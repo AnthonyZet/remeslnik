c04e87a78f9532720becb4c0539a97b72eb1d923
AnthonyZet <67970914+AnthonyZet@users.noreply.github.com>
2021-10-20 19:33:15 +0200

    tab galleries

--- js/script.js
+++ js/script.js
@@ -1,6 +1,33 @@
 const changeImage = (src) =>{
     document.getElementById("main").src = src;
 }
+const changeImage2 = (src) =>{
+  document.getElementById("main2").src = src;
+}
+
+const changeImage3 = (src) =>{
+  document.getElementById("main3").src = src;
+}
+
+/*
+Finished jobs gallery
+*/
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+  })
+})
 
 $(document).ready(function() {
   $('.hamburgerMenu').click(function(){
